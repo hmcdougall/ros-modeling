80117fe2d4fa1a9b2a637440ba8ab57d1a302146
hmcdougall <mcdoughn@miamioh.edu>
2023-09-14 15:56:23 -0400

    basic snake game

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ros-modeling
-MU Senior Design
+# Robot Operating System Modeling
+Miami University Senior Design Project
+Contributors: Hannah McDougall nad Cole Winkhart
+
+## Development
+PyGame is a required package for development. Install with:
+`python3 -m pip install pygame`
---
+++ car_sim.py
@@ -0,0 +1,122 @@
+# importing libraries
+import pygame
+import time
+
+snake_speed = 15
+
+# Window size
+window_x = 720
+window_y = 480
+
+# defining colors
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+
+# Initialising pygame
+pygame.init()
+
+# Initialise game window
+pygame.display.set_caption('Car Simulation')
+game_window = pygame.display.set_mode((window_x, window_y))
+
+# FPS (frames per second) controller
+fps = pygame.time.Clock()
+
+# defining snake default position
+snake_position = [100, 50]
+
+# defining first 4 blocks of snake body
+snake_body = [[100, 50],
+			[90, 50],
+			[80, 50],
+			[70, 50]
+			]
+
+# setting default snake direction towards
+# right
+direction = 'RIGHT'
+change_to = direction
+
+# game over function
+def game_over():
+
+	# creating font object my_font
+	my_font = pygame.font.SysFont('times new roman', 50)
+	
+	pygame.display.flip()
+	
+	# after 2 seconds we will quit the program
+	time.sleep(2)
+	
+	# deactivating pygame library
+	pygame.quit()
+	
+	# quit the program
+	quit()
+
+
+# Main Function
+while True:
+	
+	# handling key events
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_UP:
+				change_to = 'UP'
+			if event.key == pygame.K_DOWN:
+				change_to = 'DOWN'
+			if event.key == pygame.K_LEFT:
+				change_to = 'LEFT'
+			if event.key == pygame.K_RIGHT:
+				change_to = 'RIGHT'
+
+	# If two keys pressed simultaneously
+	# we don't want snake to move into two
+	# directions simultaneously
+	if change_to == 'UP' and direction != 'DOWN':
+		direction = 'UP'
+	if change_to == 'DOWN' and direction != 'UP':
+		direction = 'DOWN'
+	if change_to == 'LEFT' and direction != 'RIGHT':
+		direction = 'LEFT'
+	if change_to == 'RIGHT' and direction != 'LEFT':
+		direction = 'RIGHT'
+
+	# Moving the snake
+	if direction == 'UP':
+		snake_position[1] -= 10
+	if direction == 'DOWN':
+		snake_position[1] += 10
+	if direction == 'LEFT':
+		snake_position[0] -= 10
+	if direction == 'RIGHT':
+		snake_position[0] += 10
+
+	snake_body.insert(0, list(snake_position))
+	snake_body.pop()
+
+	game_window.fill(black)
+	
+	for pos in snake_body:
+		pygame.draw.rect(game_window, green,
+						pygame.Rect(pos[0], pos[1], 10, 10))
+
+	# Game Over conditions
+	if snake_position[0] < 0 or snake_position[0] > window_x-10:
+		game_over()
+	if snake_position[1] < 0 or snake_position[1] > window_y-10:
+		game_over()
+
+	# Touching the snake body
+	for block in snake_body[1:]:
+		if snake_position[0] == block[0] and snake_position[1] == block[1]:
+			game_over()
+
+	# Refresh game screen
+	pygame.display.update()
+
+	# Frame Per Second /Refresh Rate
+	fps.tick(snake_speed)
