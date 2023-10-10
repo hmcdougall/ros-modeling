a27f628c64d04dfbc816e1015d65416637663f0e
hmcdougall <mcdoughn@miamioh.edu>
2023-10-10 02:21:11 -0400

    added sliders for speed and angle

--- README.md
+++ README.md
@@ -3,7 +3,10 @@ Miami University Senior Design Project
 Advisor: Jim Leonard
 Contributors: Hannah McDougall and Cole Winkhart
 
-## Development
-PyGame is a required package for development. Install from terminal with:
-`python3 -m pip install pygame`
+## Requirements
+PyGame and pygame widgets are required packages for development. Install from terminal with:
+```
+python3 -m pip install pygame
+python3 -m pip install pygame_widgets
+```
 Tested on python 3.11.2, expected compatible with python 3.9+ (requires testing)
--- car_sim.py
+++ car_sim.py
@@ -2,22 +2,13 @@
 import pygame
 import time
 import math
-
-car_speed = 15
-unit_vec = 2
-theta = 0
-angle_turn = 8
+import pygame_widgets
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
 
 # Window size
-window_x = 720
-window_y = 480
-
-# defining colors
-black = pygame.Color(0, 0, 0)
-white = pygame.Color(255, 255, 255)
-red = pygame.Color(255, 0, 0)
-green = pygame.Color(0, 255, 0)
-blue = pygame.Color(0, 0, 255)
+window_x = 900
+window_y = 600
 
 # Initialising pygame
 pygame.init()
@@ -29,6 +20,15 @@ game_window = pygame.display.set_mode((window_x, window_y))
 # FPS (frames per second) controller
 fps = pygame.time.Clock()
 
+# defining colors
+black = pygame.Color(0, 0, 0)
+
+# initialize directional variables
+car_speed = 0
+unit_vec = 2
+theta = 0
+angle_turn = 4
+
 # defining car default position
 car_position = [(window_x/2), (window_y/2)]
 
@@ -36,37 +36,43 @@ car_position = [(window_x/2), (window_y/2)]
 car_body = pygame.image.load('car.png').convert_alpha()
 car = pygame.transform.rotate(car_body, theta)
 screen = pygame.display.set_mode([800,500])
+# offset point car turns around (back wheels)
 offset = -20
+center_position_x = window_x/2
+center_position_y = window_y/2
 offset_vector_x = 0
-offset_vector_y = -20
+offset_vector_y = 20
 
-# setting default car direction towards right
-direction = 'STATIONARY'
+# defining sliders
+speed_slider = Slider(screen, 30, 30, 200, 10, min=0, max=99, step=1, initial=0)
+speed_val = TextBox(screen, 35, 42, 25, 25, fontSize=20)
+speed_val.disable()  # Act as label instead of textbox
+angle_slider = Slider(screen, 270, 30, 200, 10, min=-75, max=75, step=1, initial=0)
+angle_val = TextBox(screen, 275, 42, 25, 25, fontSize=20)
+angle_val.disable()
+
+# setting default car direction forward
+direction = 'FORWARD'
 change_to = direction
 
-key_events = {pygame.K_UP: 'UP', 
-			  pygame.K_DOWN: 'DOWN',
+key_events = {pygame.K_UP: 'FORWARD', 
+			  pygame.K_DOWN: 'BACKWARD',
 			  pygame.K_LEFT: 'LEFT',
 			  pygame.K_RIGHT: 'RIGHT',
 			  pygame.K_SPACE: 'STATIONARY'} 
 
 # game over function
 def game_over():
-	# after 1 second, quit the program
-	time.sleep(1)
+	time.sleep(0.5)
 	pygame.quit()
 	quit()
 
 def rotate():
 	car = pygame.transform.rotate(car_body, theta)
 
-	# calculate new offset vector 
+	# calculate new offset vector so car continues to turn around back wheels
 	offset_vector_x = offset * math.sin(math.radians(theta))
 	offset_vector_y = offset * math.cos(math.radians(theta))
-
-    # Add the offset vector to the pivot point
-	car_position[1] += offset_vector_y
-	car_position[0] += offset_vector_x
 	return car
 
 # Main Function
@@ -79,43 +85,50 @@ while True:
 				car_speed = 0
 				print('stop')
 			elif key_events[event.key] == change_to:
-				car_speed += 15
-				print('increasing the speed')
+				car_speed += 5
+				print('increasing the speed: ', car_speed)
+				direction = change_to
 			else:
-				car_speed = 15
-				print('changing direction')
-				change_to = key_events[event.key]		
+				print('changing direction, speed = ', car_speed, ' direction = ', change_to)
+				change_to = key_events[event.key]
+			speed_slider.setValue(car_speed)
+			angle_slider.setValue(theta)
 
-	# If two keys pressed simultaneously
-	# we don't want car to move into two
-	# directions simultaneously
-	if change_to == 'UP':
-		direction = 'UP'
-	elif change_to == 'DOWN':
-		direction = 'DOWN'
-	elif change_to == 'STATIONARY':
-		0 # todo
-	elif change_to == 'LEFT' or change_to == 'RIGHT':
+	if change_to == 'LEFT' or change_to == 'RIGHT':
 		if change_to == 'LEFT':
 			theta += angle_turn
 		else:
 			theta -= angle_turn
 		change_to = "NONE"
-		# car = pygame.transform.rotate(car_body, theta)
+		car = rotate()
+	
+	theta = -1 * angle_slider.getValue()	
+	if (angle_slider.getValue() > 0):
+		change_to = 'LEFT'
+		car = rotate()
+	if (angle_slider.getValue() < 0):
+		change_to = 'RIGHT'	
 		car = rotate()
 
-
 	# Moving the car
-	if direction == 'UP':
-		car_position[1] -= unit_vec * math.cos(math.radians(theta))
-		car_position[0] -= unit_vec * math.sin(math.radians(theta))
-	if direction == 'DOWN':
-		car_position[1] += unit_vec * math.cos(math.radians(theta))
-		car_position[0] += unit_vec * math.sin(math.radians(theta))
+	if direction == 'FORWARD':
+		center_position_x -= unit_vec * math.sin(math.radians(theta)) * car_speed/5
+		center_position_y -= unit_vec * math.cos(math.radians(theta)) * car_speed/5
+		car_position[1] = offset_vector_y + center_position_y
+		car_position[0] = offset_vector_x + center_position_x
+	if direction == 'BACKWARD':
+		center_position_x += unit_vec * math.sin(math.radians(theta)) * car_speed/5
+		center_position_y += unit_vec * math.cos(math.radians(theta)) * car_speed/5
+		car_position[1] = offset_vector_y + center_position_y
+		car_position[0] = offset_vector_x + center_position_x
 
+	# render
 	game_window.fill(black)
-	screen.blit(car, (car_position[0],car_position[1]))
-	pygame.draw.circle(game_window, blue, (window_x/2, window_y/2+20), 1)
+	screen.blit(car, (car_position[0], car_position[1]))
+	pygame_widgets.update(pygame.event.get())
+	speed_val.setText(speed_slider.getValue())
+	angle_val.setText(angle_slider.getValue())
+	car_speed = speed_slider.getValue() 
     
 	pygame.display.flip()
 
@@ -128,5 +141,5 @@ while True:
 	# Refresh game screen
 	pygame.display.update()
 
-	# Frame Per Second / Refresh Rate
-	fps.tick(car_speed)
+	# screen refresh rate
+	fps.tick(20)
